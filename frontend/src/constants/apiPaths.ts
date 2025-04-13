c77ce5b8809f4e8f5e06433786e0edb3fea61779
HunterTigerX <HunterTigerX@gmail.com>
2025-04-13 11:53:51 +0300

    fix: add missing path

--- frontend/src/constants/apiPaths.ts
+++ frontend/src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 const API_PATHS = {
   product: "https://9i0kqimvjg.execute-api.eu-central-1.amazonaws.com/prod",
-  order: "https://.execute-api.eu-central-1.amazonaws.com/prod/api/profile/cart",
+  order: "https://d3i4i6d65bcscf.cloudfront.net/api/profile/cart",
   import: "https://tnj6bqz37j.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://d3i4i6d65bcscf.cloudfront.net",
