35072755cfd2aa3300335b94664dd0b8ccf0c836
HunterTigerX <HunterTigerX@gmail.com>
2025-04-18 18:32:07 +0300

    feat: updated authorization

--- backend/authorization-service/lambda/index.js
+++ backend/authorization-service/lambda/index.js
@@ -1,6 +1,9 @@
+const axios = require("axios");
+
 const handler = async (event) => {
   console.log("Event: ", JSON.stringify(event));
   console.log("event.authorizationToken", event.authorizationToken);
+
   const storedCredentials = process.env.CREDENTIALS || "";
   const login = storedCredentials.split("=")[0];
 
@@ -20,11 +23,28 @@ const handler = async (event) => {
     }
 
     const decodedString = Buffer.from(authToken, "base64").toString("ascii");
+    try {
+      console.log("authToken", authToken);
+      const profileUrl =
+        "https://wu9umi35c8.execute-api.eu-central-1.amazonaws.com/prod/api/auth/login";
+      const [username, password] = decodedString.split(":");
 
-    if (decodedString === storedCredentials) {
-      return generatePolicy(login, "Allow", event.methodArn, event.methodArn);
-    } else {
-      // Invalid credentials, deny with 403 context
+      const userExists = await axios({
+        method: "POST",
+        url: profileUrl,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          username,
+          password,
+        },
+      });
+      if (userExists.status === 200) {
+        return generatePolicy(login, "Allow", event.methodArn, event.methodArn);
+      }
+    } catch (err) {
+      console.error(err);
       return generatePolicy(login, "Deny", event.methodArn, event.methodArn);
     }
   } catch (error) {
